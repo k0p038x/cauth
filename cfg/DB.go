5a9b46fb5565bb06e7e29bf88c7b9bc21c088e93
Preetham <35539425+k0p038x@users.noreply.github.com>
2022-12-12 12:14:23 +0530

    Update db config

--- cfg/DB.go
+++ cfg/DB.go
@@ -13,7 +13,6 @@ func GetDBConfig() DBConfig {
 	val, ok := os.LookupEnv(DB_CONN_KEY)
 	if !ok {
 		fmt.Printf("%s env not set\n", DB_CONN_KEY)
-		return DBConfig{ConnURL: "postgres://krishna.preetham09:project=quiet-tree-321072;SaKdk9FjuI7y@ep-wandering-star-085518.us-east-2.aws.neon.tech/cauth?sslmode=require"}
 	}
 	return DBConfig{ConnURL: val}
 }
